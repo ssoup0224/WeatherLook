d72df5a992b3b30ed173a7d6fbe1917af50e1ce6
정성국 <ssoup0224@gmail.com>
2023-06-24 21:56:13 +0900

    Update main.dart

--- weather_look/lib/main.dart
+++ weather_look/lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:weather_look/app.dart';
+import 'package:weather_look/firebase_options.dart';
 
 Future<void> main() async {
+  //KakaoSdk.init(nativeAppKey: '66f8ad7afa5171a7d8923c9e6f94e6e1');
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  KakaoSdk.init(nativeAppKey: '66f8ad7afa5171a7d8923c9e6f94e6e1');
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const WeatherLook());
 }
