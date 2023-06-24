423003b0ea52f733d87a34383b7b6499f5724e26
정성국 <ssoup0224@gmail.com>
2023-06-24 21:55:57 +0900

    Create chatGpt.dart

---
+++ weather_look/lib/util/chatGpt.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+const apiKey = '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';
+const apiUrl = 'https://api.openai.com/v1/completions';
+
+class ChatGpt {
+  Future<String> generateText(String prompt) async {
+    String prompt2 = prompt;
+    final response = await http.post(
+      Uri.parse(apiUrl),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $apiKey'
+      },
+      body: jsonEncode({
+        "model": "text-davinci-003",
+        'prompt': prompt2,
+        'max_tokens': 1000,
+        'temperature': 0,
+        'top_p': 1,
+        'frequency_penalty': 0,
+        'presence_penalty': 0
+      }),
+    );
+    if (response.statusCode == 200) {
+      Map<String, dynamic> newresponse =
+          jsonDecode(utf8.decode(response.bodyBytes));
+      return newresponse['choices'][0]['text'];
+    } else {
+      throw Exception('Failed to get chat response');
+    }
+  }
+}
