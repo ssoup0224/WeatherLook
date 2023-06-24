a41702fa3074dc639c79bf2662c0f7bc9afd2602
정성국 <ssoup0224@gmail.com>
2023-06-24 21:55:55 +0900

    Create google_button.dart

---
+++ weather_look/lib/util/google_button.dart
@@ -0,0 +1,87 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import '../home.dart';
+import 'auth.dart';
+
+class GoogleSignInButton extends StatefulWidget {
+  const GoogleSignInButton({super.key});
+
+  @override
+  _GoogleSignInButtonState createState() => _GoogleSignInButtonState();
+}
+
+class _GoogleSignInButtonState extends State<GoogleSignInButton> {
+  bool _isSigningIn = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16.0),
+      child: _isSigningIn
+          ? const CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Color(0xffff0c4c8a)),
+            )
+          : ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFFFFFFFF),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(24),
+                ),
+                side: const BorderSide(
+                  width: 1,
+                  color: Color(0xff619bd4),
+                ),
+              ),
+              onPressed: () async {
+                setState(() {
+                  _isSigningIn = true;
+                });
+
+                User? user =
+                    await Authentication.signInWithGoogle(context: context);
+                setState(() {
+                  _isSigningIn = false;
+                });
+                if (user != null) {
+                  // ignore: use_build_context_synchronously
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => UserInfoScreen(user: user),
+                    ),
+                  );
+                }
+              },
+              child: Container(
+                width: 240,
+                height: 44,
+                decoration: const BoxDecoration(),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 18),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        width: 28,
+                        height: 28,
+                        child: Image.asset('asset/image/google_logo.png'),
+                      ),
+                      const SizedBox(
+                        width: 13,
+                      ),
+                      const Text(
+                        'Google 로그인',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Color(0xFF0C4C8A),
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+    );
+  }
+}
