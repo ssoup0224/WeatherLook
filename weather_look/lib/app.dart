66326bb25bcaa009a3cf8a42ac280965cb45ad16
정성국 <ssoup0224@gmail.com>
2023-06-24 21:56:22 +0900

    Update app.dart

--- weather_look/lib/app.dart
+++ weather_look/lib/app.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-
-import 'login.dart';
+import 'package:weather_look/gender.dart';
+import 'package:weather_look/login.dart';
+import 'package:weather_look/signup.dart';
+import 'package:weather_look/style.dart';
+import 'package:weather_look/tts.dart';
 
 class WeatherLook extends StatelessWidget {
   const WeatherLook({super.key});
@@ -12,6 +15,10 @@ class WeatherLook extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (BuildContext context) => const LoginPage(),
+        '/signup': (BuildContext context) => const SignUpPage(),
+        '/gender': (BuildContext context) => const GenderSelectPage(),
+        '/style': (BuildContext context) => const StyleSelectPage(),
+        '/tts': (BuildContext context) => TextToSpeech(),
       },
     );
   }
