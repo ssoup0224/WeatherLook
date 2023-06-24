7b57dbad7b54295f93c3a81583ec924b96ce30f6
정성국 <ssoup0224@gmail.com>
2023-06-24 21:55:59 +0900

    Create auth.dart

---
+++ weather_look/lib/util/auth.dart
@@ -0,0 +1,199 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import '../home.dart';
+
+class Authentication {
+  static Future<FirebaseApp> initializeFirebase({
+    required BuildContext context,
+  }) async {
+    FirebaseApp firebaseApp = await Firebase.initializeApp();
+
+    User? user = FirebaseAuth.instance.currentUser;
+
+    if (user != null) {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => UserInfoScreen(
+            user: user,
+          ),
+        ),
+      );
+    }
+
+    return firebaseApp;
+  }
+
+  static Future<User?> signInWithGoogle({required BuildContext context}) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+
+    final GoogleSignInAccount? googleSignInAccount =
+        await googleSignIn.signIn();
+
+    if (googleSignInAccount != null) {
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
+
+      try {
+        final UserCredential userCredential =
+            await auth.signInWithCredential(credential);
+
+        user = userCredential.user;
+
+        // Save user information in Firestore
+        await saveGoogleUserInfo(user!);
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'account-exists-with-different-credential') {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text(
+                  'The account already exists with a different credential.'),
+            ),
+          );
+        } else if (e.code == 'invalid-credential') {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text(
+                  'Error occurred while accessing credentials. Try again.'),
+            ),
+          );
+        }
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Error occurred using Google Sign-In. Try again.'),
+          ),
+        );
+      }
+    }
+
+    return user;
+  }
+
+  static Future<void> signOut({required BuildContext context}) async {
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+    await FirebaseAuth.instance.signOut();
+    await googleSignIn.signOut();
+  }
+
+  static Future<void> saveGoogleUserInfo(User user) async {
+    try {
+      final CollectionReference usersCollection = FirebaseFirestore.instance
+          .collection('allusers')
+          .doc('google')
+          .collection('users');
+      await usersCollection.doc(user.uid).set({
+        'uid': user.uid,
+        'name': user.displayName,
+        'email': user.email,
+        // Add any additional user information you want to save
+      });
+    } catch (e) {
+      print('Firestore error: $e');
+    }
+  }
+
+  static Future<User?> signUpWithEmail({
+    required BuildContext context,
+    required String email,
+    required String password,
+  }) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+
+    try {
+      final UserCredential userCredential =
+          await auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      user = userCredential.user;
+
+      // Save user information in Firestore
+      await saveRegisteredUserInfo(user!);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('The password provided is too weak.'),
+          ),
+        );
+      } else if (e.code == 'email-already-in-use') {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('An account already exists with the provided email.'),
+          ),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Error occurred during sign up. Please try again.'),
+        ),
+      );
+    }
+
+    return user;
+  }
+
+  static Future<User?> signInWithEmail({
+    required BuildContext context,
+    required String email,
+    required String password,
+  }) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+
+    try {
+      final UserCredential userCredential =
+          await auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      user = userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found' || e.code == 'wrong-password') {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Invalid email or password.'),
+          ),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Error occurred during sign in. Please try again.'),
+        ),
+      );
+    }
+
+    return user;
+  }
+
+  static Future<void> saveRegisteredUserInfo(User user) async {
+    try {
+      final CollectionReference usersCollection = FirebaseFirestore.instance
+          .collection('allusers')
+          .doc('registered')
+          .collection('users');
+      await usersCollection.doc(user.uid).set({
+        'uid': user.uid,
+        'email': user.email,
+        // Add any additional user information you want to save
+      });
+    } catch (e) {
+      print('Firestore error: $e');
+    }
+  }
+}
