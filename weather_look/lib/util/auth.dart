2ebcb1321520c1c130f7cb7d1a1a8a0481473f39
정성국 <ssoup0224@gmail.com>
2023-06-30 08:02:14 +0900

    Update auth.dart

--- weather_look/lib/util/auth.dart
+++ weather_look/lib/util/auth.dart
@@ -4,7 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-import '../home.dart';
+import '../screens/home.dart';
 
 class Authentication {
   static Future<FirebaseApp> initializeFirebase({
